97465730302ed748844053d44fc492afbea44038
Usmankhan-038 <fk514955@gmail.com>
2024-05-27 22:19:54 +0500

    update

--- cart.php
+++ cart.php
@@ -76,7 +76,7 @@ if(isset($_POST['add_to_cart']))
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
          
           <li class="nav-item">
-            <a class="nav-link" href="index.html">Home</a>
+            <a class="nav-link" href="index.php">Home</a>
           </li>
 
           <li class="nav-item">
